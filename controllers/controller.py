68f16d442b314e9e96a25d6d289eb00398e5a68a
Nathan <nathan.rendall@gmail.com>
2021-07-03 12:28:50 +0100

    computer player generator  added to game class, rudimentary game history/scoreboard page added in,

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,11 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.player import Player
-from models.game import Game
+from models.game import *
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/welcome")
 def welcome():
@@ -14,6 +15,16 @@ def welcome():
 @app.route("/play")
 def play_computer():
     return render_template("play.html")
+
+@app.route("/play", methods=["POST"])
+def challenger_entry():
+    challenger_name = request.form["player_name"]
+    challenger_choice = request.form["player_choose"]
+    challenger = Player(challenger_name, challenger_choice)
+    computer = computer_player()
+    result = judication(challenger, computer)
+    win_tracker(result)
+    return render_template("result.html", result=result, player_1=challenger, player_2=computer)
 
 @app.route("/<player_1_choice>")
 def first_choice(player_1_choice):
@@ -24,8 +35,10 @@ def first_choice(player_1_choice):
 def second_choice(player_1_choice, player_2_choice):
     player_1 = Player("Player 1", player_1_choice)
     player_2 = Player("Player 2", player_2_choice)
-    game = Game()
-    result = Game.judication(player_1, player_2)
-    # Game.victory(game, result)
-    return render_template("result.html", result=result, player_1=player_1, player_2=player_2, game=game)
+    result = judication(player_1, player_2)
+    win_tracker(result)
+    return render_template("result.html", result=result, player_1=player_1, player_2=player_2, wins=wins)
 
+@app.route("/game_history")
+def game_history():
+    return render_template("game_history.html", wins=wins)            
--- models/game.py
+++ models/game.py
@@ -1,32 +1,37 @@
 from models.player import Player
+from random import Random
 
-class Game():
+# class Game():
 
-    # def __init__(self):
-    #     self.player_1_victory_count = 0
-    #     self.player_2_victory_count = 0
+#     def __init__(self):
+#         self.player_1_win_count = 0
+#         self.player_2_win_count = 0
 
+wins=[]
 
-    def judication(player_1, player_2):
-        if player_1.choice == player_2.choice:
-            return None
-        elif player_1.choice == "rock" and player_2.choice == "paper":
-            return player_2
-        elif player_1.choice == "rock" and player_2.choice == "scissors":
-            return player_1
-        elif player_1.choice == "paper" and player_2.choice == "scissors":
-            return player_2
-        elif player_1.choice == "paper" and player_2.choice == "rock":
-            return player_1
-        elif player_1.choice == "scissors" and player_2.choice == "rock":
-            return player_2
-        elif player_1.choice == "scissors" and player_2.choice == "paper":
-            return player_1
+def judication(player_1, player_2):
+    if player_1.choice == player_2.choice:
+        return None
+    elif player_1.choice == "rock" and player_2.choice == "paper":
+        return player_2
+    elif player_1.choice == "rock" and player_2.choice == "scissors":
+        return player_1
+    elif player_1.choice == "paper" and player_2.choice == "scissors":
+        return player_2
+    elif player_1.choice == "paper" and player_2.choice == "rock":
+        return player_1
+    elif player_1.choice == "scissors" and player_2.choice == "rock":
+        return player_2
+    elif player_1.choice == "scissors" and player_2.choice == "paper":
+        return player_1
 
-    # def victory(self, judication_result):
-    #     if judication_result == None:
-    #         return
-    #     if judication_result == "player_1":
-    #         self.player_1_victory_count += 1
-    #     else:
-    #         self.player_2_victory_count += 1
+def computer_player():
+    choices = ["rock", "paper", "scissors"]
+    name = "Computer"
+    return Player(name, Random().choice(choices))
+
+def win_tracker(player):
+    if player == None:
+        wins.append("Draw")
+    else:
+        wins.append(player.name)
