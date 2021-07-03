357eb59890a8be2cd6c142f1bd7ed80416931ba6
Nathan <nathan.rendall@gmail.com>
2021-07-03 11:11:08 +0100

    welcome page with rules added, wip play page added.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,11 +1,19 @@
-from flask import render_template, request
+from flask import render_template
 from app import app
 from models.player import Player
-from models.game import judication
+from models.game import Game
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/play")
+def play_computer():
+    return render_template("play.html")
 
 @app.route("/<player_1_choice>")
 def first_choice(player_1_choice):
@@ -16,17 +24,8 @@ def first_choice(player_1_choice):
 def second_choice(player_1_choice, player_2_choice):
     player_1 = Player("Player 1", player_1_choice)
     player_2 = Player("Player 2", player_2_choice)
-    result = judication(player_1, player_2)
-    return render_template("result.html", result=result, player_1=player_1, player_2=player_2)
+    game = Game()
+    result = Game.judication(player_1, player_2)
+    # Game.victory(game, result)
+    return render_template("result.html", result=result, player_1=player_1, player_2=player_2, game=game)
 
-# @app.route("/rock")
-# def rock():
-#     return render_template("rock.html")
-
-# @app.route("/paper")
-# def paper():
-#     return render_template("paper.html")
-
-# @app.route("/scissors")
-# def scissors():
-#     return render_template("scissors.html")
--- models/game.py
+++ models/game.py
@@ -1,19 +1,32 @@
 from models.player import Player
 
+class Game():
+
+    # def __init__(self):
+    #     self.player_1_victory_count = 0
+    #     self.player_2_victory_count = 0
 
 
-def judication(player_1, player_2):
-    if player_1.choice == player_2.choice:
-        return None
-    elif player_1.choice == "rock" and player_2.choice == "paper":
-        return player_2
-    elif player_1.choice == "rock" and player_2.choice == "scissors":
-        return player_1
-    elif player_1.choice == "paper" and player_2.choice == "scissors":
-        return player_2
-    elif player_1.choice == "paper" and player_2.choice == "rock":
-        return player_1
-    elif player_1.choice == "scissors" and player_2.choice == "rock":
-        return player_2
-    elif player_1.choice == "scissors" and player_2.choice == "paper":
-        return player_1
+    def judication(player_1, player_2):
+        if player_1.choice == player_2.choice:
+            return None
+        elif player_1.choice == "rock" and player_2.choice == "paper":
+            return player_2
+        elif player_1.choice == "rock" and player_2.choice == "scissors":
+            return player_1
+        elif player_1.choice == "paper" and player_2.choice == "scissors":
+            return player_2
+        elif player_1.choice == "paper" and player_2.choice == "rock":
+            return player_1
+        elif player_1.choice == "scissors" and player_2.choice == "rock":
+            return player_2
+        elif player_1.choice == "scissors" and player_2.choice == "paper":
+            return player_1
+
+    # def victory(self, judication_result):
+    #     if judication_result == None:
+    #         return
+    #     if judication_result == "player_1":
+    #         self.player_1_victory_count += 1
+    #     else:
+    #         self.player_2_victory_count += 1
